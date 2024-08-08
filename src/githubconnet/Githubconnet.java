ded053f2aee1ccc5bc08ff700e3e00f8a74c9451
tyatya <tyatya@LAPTOP-BOR14IPJ>
2024-08-08 13:59:01 +0200

    First Commit

---
+++ src/githubconnet/Githubconnet.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package githubconnet;
+
+/**
+ *
+ * @author tyatya
+ */
+public class Githubconnet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+    
+}
